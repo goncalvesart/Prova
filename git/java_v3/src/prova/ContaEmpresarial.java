1267f4b9db30e1d1377cdf0387f08e96fec72bef
goncalvesart <152934096+goncalvesart@users.noreply.github.com>
2024-09-13 19:45:51 -0300

    Update ContaEmpresarial.java

--- git/java_v3/src/prova/ContaEmpresarial.java
+++ git/java_v3/src/prova/ContaEmpresarial.java
@@ -4,28 +4,28 @@ public class ContaEmpresarial extends Conta {
     private double limiteSaque;
     private double limiteEmprestimo;
 
-    // Construtor padrão
+
     public ContaEmpresarial() {}
 
-    // Construtor personalizado
+ 
     public ContaEmpresarial(int numeroConta, String nomeTitular, int idade, double saldo, double limiteSaque, double limiteEmprestimo) {
         super(numeroConta, nomeTitular, idade, saldo);
         this.limiteSaque = limiteSaque;
         this.limiteEmprestimo = limiteEmprestimo;
     }
 
-    // Métodos Getters e Setters
+   
     public double getLimiteSaque() { return limiteSaque; }
     public void setLimiteSaque(double limiteSaque) { this.limiteSaque = limiteSaque; }
 
     public double getLimiteEmprestimo() { return limiteEmprestimo; }
     public void setLimiteEmprestimo(double limiteEmprestimo) { this.limiteEmprestimo = limiteEmprestimo; }
 
-    // Sobrecarga de métodos
+    
     @Override
     public boolean emprestar(double valor) {
         if (valor <= limiteEmprestimo) {
-            saldo -= (valor + 50);  // Taxa de R$ 50,00
+            saldo -= (valor + 50);  
             return true;
         }
         return false;
@@ -33,7 +33,7 @@ public class ContaEmpresarial extends Conta {
 
     @Override
     public boolean sacar(double valor) {
-        if (saldo >= (valor + 5)) {  // Taxa de R$ 5,00
+        if (saldo >= (valor + 5)) {  
             saldo -= (valor + 5);
             return true;
         }
