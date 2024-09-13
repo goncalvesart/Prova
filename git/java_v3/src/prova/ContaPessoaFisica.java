cd8635025ba474f7b47f0531269fadf01a1268ee
goncalvesart <152934096+goncalvesart@users.noreply.github.com>
2024-09-13 19:46:24 -0300

    Update ContaPessoaFisica.java

--- git/java_v3/src/prova/ContaPessoaFisica.java
+++ git/java_v3/src/prova/ContaPessoaFisica.java
@@ -4,28 +4,34 @@ public class ContaPessoaFisica extends Conta {
     private double limiteSaque;
     private double limiteEmprestimo;
 
-    // Construtor padrão
+    
     public ContaPessoaFisica() {}
 
-    // Construtor personalizado
     public ContaPessoaFisica(int numeroConta, String nomeTitular, int idade, double saldo, double limiteSaque, double limiteEmprestimo) {
         super(numeroConta, nomeTitular, idade, saldo);
         this.limiteSaque = limiteSaque;
         this.limiteEmprestimo = limiteEmprestimo;
     }
 
-    // Métodos Getters e Setters
-    public double getLimiteSaque() { return limiteSaque; }
-    public void setLimiteSaque(double limiteSaque) { this.limiteSaque = limiteSaque; }
+    public double getLimiteSaque() { 
+        return limiteSaque; 
+    }
+    public void setLimiteSaque(double limiteSaque) { 
+        this.limiteSaque = limiteSaque; 
+    }
 
-    public double getLimiteEmprestimo() { return limiteEmprestimo; }
-    public void setLimiteEmprestimo(double limiteEmprestimo) { this.limiteEmprestimo = limiteEmprestimo; }
+    public double getLimiteEmprestimo() {
+        return limiteEmprestimo; 
+    }
+    public void setLimiteEmprestimo(double limiteEmprestimo) { 
+        this.limiteEmprestimo = limiteEmprestimo; 
+    }
 
-    // Sobrecarga de métodos
+    
     @Override
     public boolean emprestar(double valor) {
         if (valor <= limiteEmprestimo) {
-            saldo -= (valor + 15);  // Taxa de R$ 15,00
+            saldo -= (valor + 15);  
             return true;
         }
         return false;
@@ -33,7 +39,7 @@ public class ContaPessoaFisica extends Conta {
 
     @Override
     public boolean sacar(double valor) {
-        if (saldo >= (valor + 3)) {  // Taxa de R$ 3,00
+        if (saldo >= (valor + 3)) {  
             saldo -= (valor + 3);
             return true;
         }
